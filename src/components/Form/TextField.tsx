549cc3aa5faaf9f7868dabc8e1c0a3c10b659a61
kharann <anhkhav@gmail.com>
2019-11-30 23:09:29 +0100

    Change textfield component to work with Formik

--- src/components/Form/TextField.tsx
+++
@@ -1,9 +0,0 @@
-import { TextField as TextFieldComponent } from "@dotkomonline/design-system";
-import styled from "styled-components";
-
-const TextField = styled(TextFieldComponent)`
-  margin-bottom: 10px;
-  width: 80%;
-`;
-
-export default TextField;
---
+++ src/util/ValidaitonSchema.tsx
@@ -0,0 +1,15 @@
+import * as Yup from "yup";
+import { FormData } from "../components/Form";
+
+export const ValidationSchema = Yup.object<FormData>().shape({
+  companyName: Yup.string().required("Required"),
+  contactName: Yup.string().required("Required"),
+  companyMail: Yup.string()
+    .email("Invalid mail")
+    .required("Required"),
+  phone: Yup.string()
+    .matches(/([ +0-9]+)/, "A phone number should only contain digits!")
+    .min(8, "Too short!")
+    .max(10, "Too long!")
+    .required("Required")
+});
