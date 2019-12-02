f517540eaa301980c545fe6a587122d0d299c8c1
kharann <anhkhav@gmail.com>
2019-12-02 23:34:10 +0100

    fix the eslint issue

--- src/components/Form/Inputs/TextField.tsx
+++ src/components/Form/Inputs/TextField.tsx
@@ -5,7 +5,9 @@ import { TextField as DsTextField } from '@dotkomonline/design-system';
 const TextField: FC<DsTextField> = (props) => {
   const [field, meta] = useField<FormData>(props);
   return (
-    <DsTextField errorMessage={meta.error && meta.touched ? meta.error : null} status={meta.touched ? (meta.error ? 'error' : 'success') : null}
+    <DsTextField
+      errorMessage={meta.error && meta.touched ? meta.error : null}
+      status={meta.touched ? (meta.error ? 'error' : 'success') : null}
       {...field}
       {...props}
     />
