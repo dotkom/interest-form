5a3143083a79e5e9599c469384380aa0aa59dd34
kharann <anhkhav@gmail.com>
2019-12-02 23:59:35 +0100

    revert the eslint error i introduced

--- src/components/Form/Inputs/TextField.tsx
+++ src/components/Form/Inputs/TextField.tsx
@@ -1,11 +1,13 @@
-  import { useField } from 'formik';
+import { useField } from 'formik';
 import React, { FC } from 'react';
 import { TextField as DsTextField } from '@dotkomonline/design-system';
 
 const TextField: FC<DsTextField> = (props) => {
   const [field, meta] = useField<FormData>(props);
   return (
-    <DsTextField errorMessage={meta.error && meta.touched ? meta.error : null} status={meta.touched ? (meta.error ? 'error' : 'success') : null}
+    <DsTextField
+      errorMessage={meta.error && meta.touched ? meta.error : null}
+      status={meta.touched ? (meta.error ? 'error' : 'success') : null}
       {...field}
       {...props}
     />
