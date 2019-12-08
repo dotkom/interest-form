0abc71f4c32d76169e90cbda84a0d21518e4a92a
kharann <anhkhav@gmail.com>
2019-12-08 18:02:10 +0100

    Change validation schema

--- src/util/ValidaitonSchema.tsx
+++ src/util/ValidaitonSchema.tsx
@@ -5,11 +5,11 @@ export const ValidationSchema = Yup.object<FormData>().shape({
   companyName: Yup.string().required('Required'),
   contactName: Yup.string().required('Required'),
   companyMail: Yup.string()
-    .email('Invalid mail')
-    .required('Required'),
+    .required('Required')
+    .email('Invalid mail'),
   phone: Yup.string()
+    .required('Required')
     .matches(/([ +0-9]+)/, 'A phone number should only contain digits!')
     .min(8, 'Too short!')
-    .max(10, 'Too long!')
-    .required('Required'),
+    .max(10, 'Too long!'),
 });
