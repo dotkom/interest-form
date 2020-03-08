27146d408d0757945f348d3e6459da43c334c9d4
kharann <anhkhav@gmail.com>
2020-03-08 18:09:51 +0100

    disable submit button when submit succeed

--- src/components/Form/Areas/SubmitArea.tsx
+++ src/components/Form/Areas/SubmitArea.tsx
@@ -18,7 +18,7 @@ const SubmitArea = ({ onClick, isSubmitting, submitted }: SubmitAreaProps) => {
           {submitted ? <Paragraph>Din interesse har blitt sendt inn!</Paragraph> : null}
         </S.SpinnerDiv>
         <S.ButtonDiv>
-          <Button color="primary" value="Send inn" onClick={onClick} />
+          <Button color="primary" value="Send inn" onClick={onClick} disabled={submitted} />
         </S.ButtonDiv>
       </S.Wrapper>
     </Area>
