93e616f6a75d87b8dc56f298c72e6b6bdd576285
kharann <anhkhav@gmail.com>
2020-10-20 00:41:00 +0200

    Move interest text out of te html

--- lambda/src/util/MailFormatters.ts
+++ lambda/src/util/MailFormatters.ts
@@ -5,6 +5,7 @@ export const getFormattedData = (form: FormData, isConfirmMail: boolean) => {
   const intro = isConfirmMail
     ? 'Vi vil meddele at deres interesse med følgende data har blitt sendt inn:'
     : 'En bedrift har meldt interesse';
+  const interestText = interests.map((interest) => `<li>${interest}</li>`);
   return `
       <p>${intro}</p>
       <p><strong>Bedrift</strong></p>
@@ -15,7 +16,7 @@ export const getFormattedData = (form: FormData, isConfirmMail: boolean) => {
       <p>Telefon: ${phone} <br><br></p>
       <p><strong>Interesser</strong></p>
       <ul>
-      ${interests.map((interest) => `<li>${interest}</li>`)}
+      ${interestText}
       </ul><br><br>
       <p><strong>Kommentarer</strong></p>
       <p>${comments}</p>
