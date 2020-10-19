9a5f977ac810db83743ab1c0d0c87496e0a826ca
kharann <anhkhav@gmail.com>
2020-10-20 01:49:31 +0200

    update validation schema to include max lenght of characters

--- common/ValidaitonSchema.ts
+++ common/ValidaitonSchema.ts
@@ -2,15 +2,22 @@ import * as Yup from 'yup';
 import { FormData } from './FormData';
 
 export const ValidationSchema = Yup.object<FormData>().shape({
-  companyName: Yup.string().required('Dette feltet er påkrevd'),
-  contactName: Yup.string().required('Dette feltet er påkrevd'),
+  companyName: Yup.string()
+    .required('Dette feltet er påkrevd')
+    .max(250, 'Navnet er for langt'),
+  contactName: Yup.string()
+    .required('Dette feltet er påkrevd')
+    .max(150, 'Navnet er for langt'),
   contactMail: Yup.string()
     .required('Dette feltet er påkrevd')
-    .email('Ugyldig format på mailen'),
+    .email('Ugyldig format på mailen')
+    .max(200, 'Mailen er for lang'),
   phone: Yup.string()
     .required('Dette feltet er påkrevd')
     .matches(/([\+]{0,1}[0-9])/, 'Telefonnummer er ugyldig og bør kun inneholde tall og maks 1 "+"')
     .min(8, 'Telefonnummer for kort!')
     .max(11, 'Telefonnummeret er for langt!'),
-  comments: Yup.string().required('Dette feltet er påkrevd'),
+  comments: Yup.string()
+    .required('Dette feltet er påkrevd')
+    .max(2000, 'Prøv å skriv under 2000 tegn'),
 });
