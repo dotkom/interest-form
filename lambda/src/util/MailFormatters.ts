0e3fd8e573c8edf313cadb559a85b731b7d6b365
kharann <anhkhav@gmail.com>
2020-10-20 00:13:28 +0200

    Change mail formatter and cahnge yarn to npm

--- lambda/src/util/MailFormatters.ts
+++ lambda/src/util/MailFormatters.ts
@@ -1,24 +1,23 @@
-import { FormData } from '../../../../src/models/Form/Form';
+import { FormData } from '../../../common/FormData';
 
 export const getFormattedData = (form: FormData, isConfirmMail: boolean) => {
   const { companyName, comments, contactName, contactMail, phone, interests } = form;
+  const intro = isConfirmMail
+    ? 'Vi vil meddele at deres interesse med følgende data har blitt sendt inn:'
+    : 'En bedrift har meldt interesse';
   return `
-    <p>
-    ${
-      isConfirmMail
-        ? 'Vi vil meddele at deres interesse med følgende data har blitt sendt inn:'
-        : 'En bedrift har meldt interesse'
-    }<br>
-    <b>Bedrift</b><br>
-    Bedriftsnavn: ${companyName}<br><br>
-    <b>Kontaktperson</b><br>
-    Navn: ${contactName}<br>
-    E-post: ${contactMail}<br>
-    Telefon: ${phone}<br><br>
-    <b>Interesser</b><br><br>
-    <ul>${interests.map((interest: string) => `<li>${interest}</li>`).join('\n')}</ul><br><br>
-    <b>Kommentar</b><br>
-    <p>${comments}</p><br>
-    </p>
+      <p>${intro}</p>
+      <p><strong>Bedrift</strong></p>
+      <p>Bedriftsnavn: ${companyName}<br><br></p>
+      <p><strong>Kontaktperson</strong></p>
+      <p>Navn: ${contactName}</p>
+      <p>E-post: ${contactMail}</p>
+      <p>Telefon: ${phone} <br><br></p>
+      <p><strong>Interesser</strong></p>
+      <ul>
+      ${interests.map((interest) => `<li>${interest}</li>`)}
+      </ul><br><br>
+      <p><strong>Kommentarer</strong></p>
+      <p>${comments}</p>
   `;
 };
