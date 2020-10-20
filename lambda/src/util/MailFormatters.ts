b8649774d140b2e9272f60cc21dfca7188388656
kharann <anhkhav@gmail.com>
2020-10-20 02:02:04 +0200

    Add slug converter

--- lambda/src/util/MailFormatters.ts
+++ lambda/src/util/MailFormatters.ts
@@ -1,11 +1,30 @@
 import { FormData } from '../../../common/FormData';
 
+const slugToText = (slug: string): string => {
+  switch (slug) {
+    case 'bedpres':
+      return 'Bedriftsarrangement';
+    case 'kurs':
+      return 'kurs';
+    case 'offline':
+      return 'Annonse i Offline';
+    case 'online_ad':
+      return 'Stillingsutlysning';
+    case 'techtalks':
+      return 'Tech talks';
+    case 'excursion':
+      return 'IT-ekskrusjon';
+    default:
+      return '';
+  }
+};
+
 export const getFormattedData = (form: FormData, isConfirmMail: boolean) => {
   const { companyName, comments, contactName, contactMail, phone, interests } = form;
   const intro = isConfirmMail
     ? 'Vi vil meddele at deres interesse med følgende data har blitt sendt inn:'
     : 'En bedrift har meldt interesse';
-  const interestText = interests.map((interest) => `<li>${interest}</li>`);
+  const interestText = interests.map((interest) => `<li>${slugToText(interest)}</li>`).join('');
   return `
       <p>${intro}</p>
       <p><strong>Bedrift</strong></p>
@@ -16,7 +35,7 @@ export const getFormattedData = (form: FormData, isConfirmMail: boolean) => {
       <p>Telefon: ${phone} <br><br></p>
       <p><strong>Interesser</strong></p>
       <ul>
-      ${interestText}
+      ${interests ? interestText : 'Bedriften har ikke valgt noen interesser'}
       </ul><br><br>
       <p><strong>Kommentarer</strong></p>
       <p>${comments}</p>
