fbec691b62b8660c13b265075269ddcfa34257ba
kharann <anhkhav@gmail.com>
2020-10-20 23:23:25 +0200

    update validation schema

--- common/ValidaitonSchema.ts
+++ common/ValidaitonSchema.ts
@@ -14,7 +14,7 @@ export const ValidationSchema = Yup.object<FormData>().shape({
     .max(200, 'Mailen er for lang'),
   phone: Yup.string()
     .required('Dette feltet er påkrevd')
-    .matches(/([\+]{0,1}[0-9])/, 'Telefonnummer er ugyldig og bør kun inneholde tall og maks 1 "+"')
+    .matches(/([\+]{0,1}[0-9\s]+)/, 'Telefonnummer er ugyldig og bør kun inneholde tall og maks 1 "+"')
     .min(8, 'Telefonnummer for kort!')
     .max(16, 'Telefonnummeret er for langt!'),
   comments: Yup.string()
