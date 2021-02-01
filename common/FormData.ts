3dcb6cdb9d53a0115b7c5cabac68b34c36c838fe
kharann <anhkhav@gmail.com>
2021-02-01 14:37:16 +0100

    Allow null type on comments

--- common/FormData.ts
+++ common/FormData.ts
@@ -4,7 +4,7 @@ export interface FormData {
   contactMail: string;
   phone: string;
   interests: Interest[];
-  comments: string;
+  comments: string | null;
 }
 
 export type Interest = 'bedpres' | 'kurs' | 'offline' | 'online_ad' | 'techtalks' | 'excursion';
