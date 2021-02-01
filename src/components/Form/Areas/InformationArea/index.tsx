c5e610a270309038e5968ae5cb7899e413f92e97
kharann <anhkhav@gmail.com>
2021-02-01 13:56:11 +0100

    Update form information to newest

---
+++ src/components/Form/Areas/InformationArea/index.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import styled from 'styled-components';
+import { colors, md } from '@dotkomonline/design-system';
+import Area from '../../../Area';
+import { introText, productDetails } from './informationText';
+
+const InformationArea = () => {
+  return (
+    <Area>
+      <S.Wrapper>
+        {md`${introText}`}
+        <S.Details>
+          <summary>Les mer om produkter</summary>
+          {md`${productDetails}`}
+        </S.Details>
+      </S.Wrapper>
+    </Area>
+  );
+};
+
+const S = {
+  Wrapper: styled.div`
+    font-size: 1rem;
+    padding: 15px 0px;
+    line-height: 1.5;
+    border-width: 1px 0px;
+    border-color: ${colors.grayDarken30};
+    border-style: solid;
+  `,
+  Details: styled.details`
+    margin: 1.25rem 0;
+    summary {
+      margin-bottom: 1rem;
+    }
+  `,
+};
+
+export default InformationArea;
---
+++ src/components/Form/Areas/InformationArea/informationText.ts
@@ -0,0 +1,61 @@
+export const introText = `
+Dette skjemaet skal brukes til å melde interesse for samarbeid med Online.
+
+Når du huker av i en sjekkboks vil vi notere deres interesse og ta dette i betraktning under planlegging av arrangementet. 
+I skjemaet har du også mulighet til å legge igjen en kommentar eller spørsmål.
+Innholdet vil bli sendt på mail til bedriftskomiteen, og om dere har noen spørsmål vil disse bli besvart så fort det lar seg gjøre. Det er mulig å fylle ut skjemaet flere ganger.
+ 
+Merk at skjemaet kun er for å melde interesse og fungerer da ikke som påmelding.
+
+Andre henvendelser kan sendes på mail til bedriftskontakt@online.ntnu.no.
+`;
+
+export const productDetails = `
+#### Tech Talks
+
+Tech Talks har den hensikt å være et faglig arrangement for inspirasjon og faglig påfyll fra
+næringslivet, samt at det skal gi inntekt til ekskursjonskomiteen som arrangerer den årlige
+ekskursjonen for informatikkstudentene ved NTNU. Arrangementet pleier å finne sted en
+gang i midten av februar.
+
+Arrangementet er lagt opp slik at flere bedrifter holder foredrag, workshops og lyntaler hvor
+de kan få presentert seg selv og vise fram sin faglige styrke. Dette vil være spredt utover
+hele dagen på et frokostseminar, lunsj-event eller kvelds-event. På kveldstid arrangeres det
+en bankett med middag og mingling på et lokale i Trondheim. Formatet på arrangementet
+kan variere fra år til år.
+
+Vi begynner å kontakte aktuelle bedrifter på slutten av høstsemesteret.
+
+#### Annonseplass i Offline
+
+Offline er linjeforeningsmagasinet til Online. Det er skrevet og produsert av Onlines Profil- og
+aviskomité. Offline blir lest av våre studenter og i tillegg sendt ut til bedrifter. Det lages to
+utgaver hvert semester. Som bedrift får dere tilbud om spalte, helsides eller halvsides
+annonse. Annonsen kan inneholde det dere selv ønsker alt etter hvordan dere ønsker å
+profilere dere. Det trenger ikke være en tradisjonell annonse, men kan f. eks være en liten
+novelle, tips og triks eller andre ting bedriften vil skrive om.
+
+#### ITEX, IT-ekskursjonen for 4. og 5. klasse til Oslo høsten 2020
+
+I månedsskifte august/september arrangeres den årlige IT-ekskursjonen til Oslo for
+masterstudenter i informatikk ved Norges teknisk- naturvitenskapelige universitet (NTNU) for
+å besøke bedrifter. Formålet er å reise på besøk til spennende og aktuelle IT-bedrifter for å
+få bedre kjennskap og forhold til potensielle arbeidsgivere.
+
+Det arrangeres 2-3 bedriftsbesøk hver dag, hvor det vil være to eller tre besøk studentene
+kan velge mellom på dagtid, og et felles besøk på kveldstid. På dagsbesøkene vil vi enten
+dele gruppen i to eller tre slik at studentene blir jevnt fordelt mellom bedriftene.
+
+Under besøket vil dere som bedrift stå fritt til hva slags opplegg dere ønsker å gjennomføre.
+Det kan være bedriftspresentasjoner, faglige kurs, casearbeid, konkurranse, sosiale
+aktiviteter, eller annet som kan gi innsikt i hvordan det er å jobbe hos dere. I tillegg ønsker vi
+å spise et måltid sammen med dere.
+
+#### Stillingsannonse på våre nettsider
+
+Vi har en karriereside [0] på våre nettsider der vi legger ut stillingsannonser for bedrifter som
+ønsker det. Det kan sendes en mail til bedriftskontrakt@online.ntnu.no om dette er av
+interesse.
+
+[0] - https://online.ntnu.no/career
+          `;
